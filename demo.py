f07b90f0b36b6b49692d67401db508d49e943e9e
Ryan Young <ry525525@gmail.com>
2014-01-10 10:41:16 -0500

    I made a mess of things

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import roslib; roslib.load_manifest('BiotacPushDemo')
+import roslib; roslib.load_manifest('BioTacPushDemo')
 import rospy
 import time
 from hubomsg.msg import *
@@ -8,8 +8,8 @@ from biotac_sensors.msg import *
 #Author: John Maloney
 
 # Constants determining how much pressure is considered not touching or pushing
-PRESSURE_TOUCHING = 2100 # This number is untested and not at all final
-PRESSURE_PUSHING = 3000 # This number is untested and not at all final
+PRESSURE_TOUCHING = 2000 # This number is untested and not at all final
+PRESSURE_PUSHING = 2100 # This number is untested and not at all final
 
 # Constants for joint limits and position increment
 RSP_OUT = -1.0
@@ -23,24 +23,26 @@ RSP_INCREMENT = .01 # Amount RSP changes. REP is a function of REP so they stay 
 class Demo:
     def __init__(self):
         rospy.init_node("BioTacDemo")
-        rospy.Subscriber("biotac_pub", BioTacHand, self.sense)
         self.pub = rospy.Publisher("Maestro/Control", PythonMessage) # Sends position commands
-        self.count = 0
-
-        # Command arm to go to start position
+        self.RSP = RSP_OUT
+        self.REP = REP_OUT
         print "Moving arm to start position"
-        self.pub.publish("RSP REP", "position position", str(RSP_OUT) + " " + str(REP_OUT), "")
+        #self.pub.publish("RSP REP", "position position", str(RSP_OUT) + " " + str(REP_OUT), "")
+        rospy.Subscriber("biotac_pub", BioTacHand, self.sense)
+        self.count = 0
+        # Command arm to go to start position
         time.sleep(10) # Sleep to allow arm to move to start position
+        self.pub.publish("RSP REP", "position position", str(RSP_OUT) + " " + str(REP_OUT), "")
         print "Starting demo"
-        self.rsp = RSP_OUT
-        self.rep = REP_OUT
+
 
         rospy.spin()
 
     def sense(self, data):
-        btdata = data.bt_datarepNew
+        btdata = data.bt_data
         pressure = btdata[0].pdc_data
-        print pressure
+        #self.pub.publish("RSP REP", "position position", ".5 .5", "")
+        #print pressure
 
         # Sensor is not being touched. Extend hand outward
         if(pressure < PRESSURE_TOUCHING):
@@ -54,6 +56,7 @@ class Demo:
         rspNew = self.RSP + RSP_INCREMENT*direction
         repNew = REP_RANGE/RSP_RANGE*(self.RSP - RSP_IN) + REP_IN
         if(rspNew > RSP_OUT and rspNew < RSP_IN and repNew < REP_OUT and repNew > REP_IN): # Make sure command positions are within arm in/out bounds
+            print "Made it in here"
             self.pub.publish("RSP REP", "position position", str(rspNew) + " " + str(repNew), "")
             self.RSP = rspNew
             self.REP = repNew
