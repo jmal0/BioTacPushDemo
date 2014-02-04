a8e7038b55f572fb079a63bf36fa8cd1f4f5b371
John Maloney <jmal0320@gmail.com>
2014-02-04 10:24:38 -0500

    Adding thresholds, averaging

--- demo.py
+++ demo.py
@@ -5,38 +5,41 @@ import rospy
 import time
 from hubomsg.msg import *
 from biotac_sensors.msg import *
-#Author: John Maloney
 
 # Constants determining how much pressure is considered not touching or pushing
-PRESSURE_TOUCHING = 2000
-PRESSURE_PUSHING = 2100
+PRESSURE_TOUCHING_LOW = 1950
+PRESSURE_TOUCHING_HIGH = 2000
+PRESSURE_PUSHING_LOW = 2050
+PRESSURE_PUSHING_HIGH = 2100
 
-# Constants for joint limits and position increment
+# Constants for joint limits
 RSP_OUT = -1.38
 RSP_IN = 0
 RSP_RANGE = RSP_IN - RSP_OUT
 REP_OUT = 0
 REP_IN = -1.34
 REP_RANGE = REP_OUT - REP_IN
-RSP_INCREMENT = .01 # Amount RSP changes. REP is a function of RSP so they stay synchronized ## This number is untested
-ID_NUM = 10
+
+RSP_INCREMENT = .05 # Amount RSP changes. REP is a function of RSP so they stay synchronized
+NUM = 20 # Number of messages to receive before publishing a position command
 
 class Demo:
     def __init__(self):
         rospy.init_node("BioTacDemo")
-
-        self.pub = rospy.Publisher("Maestro/Control", MaestroCommand) # Sends position commands
         
-        self.count = 0
+        self.pub = rospy.Publisher("Maestro/Control", PythonMessage) # Sends position commands
+        
+        self.count = 1
         self.RSP = RSP_IN
         self.REP = REP_IN
+        self.pressure = 0.0
+        self.last = -1
         time.sleep(1)
         
         # Command arm to go to start position
-
-         # Sleep to allow arm to move to start position
-        self.pub.publish("RSP REP", "position position", str(RSP_IN) + " " + str(REP_IN), "", ID_NUM)
-        time.sleep(2)
+        print "Moving arm to start position"
+        self.pub.publish("RSP REP RF2", "position position position", str(RSP_IN) + " " + str(REP_IN) + ".8", "")
+        time.sleep(3)
         print "Starting demo"
 
         rospy.Subscriber("biotac_pub", BioTacHand, self.sense)
@@ -44,19 +47,39 @@ class Demo:
         rospy.spin()
 
     def sense(self, data):
-        if self.count == 5:
-            btdata = data.bt_data
-            pressure = btdata[0].pdc_data
-            print pressure
-
+        btdata = data.bt_data
+        self.pressure += btdata[0].pdc_data
+        
+        if self.count == NUM:
+            ave = self.pressure/NUM
+            print ave
+            
             # Sensor is not being touched. Extend hand outward
-            if(pressure < PRESSURE_TOUCHING):
+            if ave < PRESSURE_TOUCHING_LOW:
+                self.last = -1
                 self.moveArm(-1)
+            # Between touching and not touching
+            elif ave < PRESSURE_TOUCHING_HIGH:
+                if self.last == -1:
+                    self.moveArm(self.last)
+                else:
+                    self.last = 0
+            # Sensor is being touched but not pushed. Do not move hand
+            elif(ave < PRESSURE_PUSHING_LOW):
+                self.last = 0
+            # Between touching and pushing
+            elif(ave < PRESSURE_PUSHING_HIGH)
+                if self.last == 1:
+                    self.moveArm(self.last)
+                else:
+                    self.last = 0
             # Sensor is being pushed. Retract hand
-            elif(pressure > PRESSURE_PUSHING):
+            else:
+                self.last = 1
                 self.moveArm(1)
-            # Else, sensor is being touched but not pushed. Do not move hand
-            self.count = 0
+            
+            self.count = 1
+            self.pressure = 0
         else:
             self.count += 1
 
@@ -65,8 +88,8 @@ class Demo:
         repNew = -REP_RANGE/RSP_RANGE*(rspNew - RSP_IN) + REP_IN
         print "RSP: " + str(rspNew)
         print "REP: " + str(repNew)
-        if(rspNew > RSP_OUT and rspNew < RSP_IN and repNew < REP_OUT and repNew > REP_IN): # Make sure command positions are within arm in/out boundsb
-            self.pub.publish("RSP REP", "position position", str(rspNew) + " " + str(repNew), "", ID_NUM)
+        if(rspNew > RSP_OUT and rspNew < RSP_IN and repNew < REP_OUT and repNew > REP_IN): # Make sure command positions are within arm in/out bounds
+            self.pub.publish("RSP REP", "position position", str(rspNew) + " " + str(repNew), "")
             self.RSP = rspNew
             self.REP = repNew
                 
@@ -75,4 +98,6 @@ if __name__ == '__main__':
     try:
         reactiveTouchdemo = Demo()
     except KeyboardInterrupt:
+        # Is this even doing anything?
+        print "Yes, this is doing something"
         sys.exit()
